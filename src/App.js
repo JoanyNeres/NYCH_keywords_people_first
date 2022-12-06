c0162e3a777761c483c8f87bba05a02f32210ba0
joanyneres <joanybneres@gmail.com>
2022-12-06 11:02:32 -0500

    new rules

--- src/App.js
+++ src/App.js
@@ -1,96 +1,53 @@
 import './App.css';
 import React, { useState } from 'react';
+import { Statements } from './Components/Statements'
+import statements from './Assets/statementsList'
+
 
 function App() {
 
-  const statements = [
-    {
-        sentence: "1. Personas are representations of your clients that will help you ________ with them.",
-        word: "EMPATHIZE",
-        statement: "1. Personas are representations of your clients that will help you empathize with them."
-    },
-    {
-        sentence: "2. A persona represents a particular group and not a single ________.",
-        word: "PERSON",
-        statement: "2. A persona represents a particular group and not a single person."
-    },
-    {
-        sentence: "3. The key pieces of information that go to your personas depends on the ________ of the experience you want to create.",
-        word: "CONTEXT",
-        statement: "3. The key pieces of information that go to your personas depends on the context of the experience you want to create."
-    },
-    {
-        sentence: "4. Fictional name, photo, age, occupation, goals and skills are common ________ included in personas.",
-        word: "COMPONENT",
-        statement: "4. Fictional name, photo, age, occupation, goals and skills are common components included in personas."
-    },
-    {
-        sentence: "5. Personas can help you make ________ through the eyes of your target participants.",
-        word: "DECISIONS",
-        statement: "5. Personas can help you make decisions through the eyes of your target participants."
-    },
-    {
-        sentence: "6. It's important to get feedback from you colleagues and ______ your personas.",
-        word: "MODIFY",
-        statement: "6. It's important to get feedback from you colleagues and modify your personas."
-    }
-  ]
-
   const [i, setIndex] = useState(0)
 
-  const[statementsBlock, setStatementsBlock] = useState([])
-
-  const [url, setUrl] = useState("")
-
-  
+  const [buttonsDisplay, setButtonsDisplay] = useState(true)
 
   const clickAnswer = (event) => {
     event.preventDefault()
-    
-    if ((document.querySelector('button:checked').id) === i.toString()){
-      document.getElementById(i).hidden=true
-      setStatementsBlock(statementsBlock.push(
-        <li>
-        <p className="statements">{statements[i].statement}</p>
-        </li>
-      ))
-      setIndex(i+1)
-      if ((document.getElementById(0).hidden=true) && (document.getElementById(1).hidden=true)
-        && (document.getElementById(2).hidden=true) && (document.getElementById(3).hidden=true)
-        && (document.getElementById(4).hidden=true) && (document.getElementById(5).hidden=true)){
-          setUrl("https://cdn.discordapp.com/attachments/1016745958727491615/1049060879959863437/Profiling-pana.png")
-        }
+    if (Number.parseInt(event.target.id) === i){
+      const buttonSelected = document.getElementById(i.toString())
+      buttonSelected.style.visibility = "hidden"
+      if (i === 5){
+        setButtonsDisplay(false)
+      }
+      else {
+        setIndex(i+1)
+      }
     }
-}
-
-
-
+  }
 
   return (
-    <div className="App">
-      
-      <h3 className="statement">{statements[i].sentence}</h3>
-
-      
-      <p className="instruction">Click in the correct word below to fill the gap:</p>
-      {{url} === "" ?
-      <div className="buttons_box">  
-          <div className="buttons">
-            <button id="3" onClick={clickAnswer}>{statements[3].word}</button>
-            <button id="2" onClick={clickAnswer}>{statements[2].word}</button>
-            <button id="0" onClick={clickAnswer}>{statements[0].word}</button>
-          </div>
-          <div className="buttons">
-            <button id="4" onClick={clickAnswer}>{statements[4].word}</button>
-            <button id="5" onClick={clickAnswer}>{statements[5].word}</button>
-            <button id="1" onClick={clickAnswer}>{statements[1].word}</button>
-          </div>
-      </div>
+    <div className="box_content">
+      {buttonsDisplay ?
+      <>
+        <h3 className="statement_incomplete">{statements[i].sentence}</h3>
+        <p className="instruction">Click in the correct word below to fill the gap:</p>
+        <div className="buttons_box">  
+            <div className="buttons_row">
+              <button id="3" onClick={clickAnswer}>{statements[3].word}</button>
+              <button id="2" onClick={clickAnswer}>{statements[2].word}</button>
+              <button id="0" onClick={clickAnswer}>{statements[0].word}</button>
+            </div>
+            <div className="buttons_row">
+              <button id="4" onClick={clickAnswer}>{statements[4].word}</button>
+              <button id="5" onClick={clickAnswer}>{statements[5].word}</button>
+              <button id="1" onClick={clickAnswer}>{statements[1].word}</button>
+            </div>
+        </div>
+      </>
       :
-      <img className="image-end" alt="illustration" src={url} />}
-      <ul>
-        {statementsBlock}
-      </ul>
+      <Statements 
+        statements={statements}
+      />
+      }
     </div>
   );
 }
---
+++ src/Components/Statements/index.jsx
@@ -0,0 +1,45 @@
+import './index.css';
+
+export function Statements(props){
+
+    const refreshPage = () => { 
+        window.location.reload(); 
+    }  
+    
+    return(
+        <div className="box_content">
+            <img className="figure_end" src="https://cdn.discordapp.com/attachments/1016745958727491615/1049064045673730178/Profile_data-pana.png"
+             alt="Persona illustration" />
+            <h2>Yeah! You got it, well done!</h2>
+            <ul className="statements_list">
+                <li>
+                    <p>{props.statements[0].statement}</p>
+                </li>
+                
+                <li>
+                    <p>{props.statements[1].statement}</p>
+                </li>
+                
+                <li>
+                    <p>{props.statements[2].statement}</p>
+                </li>
+
+                <li>
+                    <p>{props.statements[3].statement}</p>
+                </li>
+                
+                <li>
+                    <p>{props.statements[4].statement}</p>
+                </li>
+
+                <li>
+                    <p>{props.statements[5].statement}</p>
+                </li>
+            </ul>
+            <button onClick={refreshPage}
+                className="button_restart">
+                RESTART
+            </button>
+        </div>
+    )
+}
